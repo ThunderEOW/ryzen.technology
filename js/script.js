d57dfa47300e5c37c2acb75ae4a3e1dda5fbed57
ThunderEOW <matyasengelen@hotmail.com>
2020-10-13 22:38:12 +0200

    Update script.js
    Added a faster way to do it

--- js/script.js
+++ js/script.js
@@ -1,1 +1,3 @@
 document.getElementById('DomainName').innerHTML = window.location.hostname;
+// bro this is dogggg
+document.getElementById('DomainName').innerHTML = window.location.hostname; //do it like this
